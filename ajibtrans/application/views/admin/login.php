8f77045a6978d98878832cca298c98c9e86574f5
Afif Nuruddin Maisaroh <afifnuruddinmaisaroh@gmail.com>
2023-11-23 20:55:57 +0700

    hapus assets sneat dan config

--- ajibtrans/application/views/admin/login.php
+++ ajibtrans/application/views/admin/login.php
@@ -22,30 +22,27 @@
         href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet" />
 
+     <!-- Icons. Uncomment required icon fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com/" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/fonts/boxicons.css" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/css/core.css"
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/core.css"
         class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/css/theme-default.css"
-        class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
     <link rel="stylesheet"
-        href="<?= base_url('assets/sneat') ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/css/pages/page-auth.css" />
-    <!-- Helpers -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/helpers.js"></script>
-
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/js/config.js"></script>
-</head>
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/theme-default.css"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/css/demo.css" />
+    <link rel="stylesheet"
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet"
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/pages/page-auth.css" />
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/helpers.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/js/config.js"></script>
 
 <body>
     <!-- Content -->
@@ -102,27 +99,14 @@
         </div>
     </div>
 
-    <!-- / Content -->
-
-
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/popper/popper.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/bootstrap.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-
-    <!-- Main JS -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/js/main.js"></script>
-
-    <!-- Page JS -->
-
-    <!-- Place this tag in your head or just before your close body tag. -->
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/popper/popper.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/bootstrap.js"></script>
+    <script
+        src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js">
+    </script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/menu.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/js/main.js"></script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
--- ajibtrans/application/views/template.php
+++ ajibtrans/application/views/template.php
@@ -15,43 +15,29 @@
       name="viewport"
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
-
     <title><?= $judul_halaman ?></title>
-
     <meta name="description" content="" />
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/sneat') ?>/assets/img/favicon/logo2.png" />
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com/" />
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/fonts/boxicons.css" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/css/demo.css" />
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
-    <link rel="stylesheet" href="<?= base_url('assets/sneat') ?>/assets/vendor/libs/apex-charts/apex-charts.css" />
-
-    <!-- Page CSS -->
-
-    <!-- Helpers -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/helpers.js"></script>
-
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/js/config.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/core.css"
+        class="template-customizer-core-css" />
+    <link rel="stylesheet"
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/theme-default.css"
+        class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/css/demo.css" />
+    <link rel="stylesheet"
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet"
+        href="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/css/pages/page-auth.css" />
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/helpers.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/js/config.js"></script>
   </head>
 
   <body>
@@ -209,28 +195,14 @@
       <div class="layout-overlay layout-menu-toggle"></div>
     </div>
     <!-- / Layout wrapper -->
-
-
-    <!-- Core JS -->
-    <!-- build:js assets/vendor/js/core.js -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/popper/popper.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/bootstrap.js"></script>
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/js/menu.js"></script>
-    <!-- endbuild -->
-
-    <!-- Vendors JS -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/vendor/libs/apex-charts/apexcharts.js"></script>
-
-    <!-- Main JS -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/js/main.js"></script>
-
-    <!-- Page JS -->
-    <script src="<?= base_url('assets/sneat') ?>/assets/js/dashboards-analytics.js"></script>
-
-    <!-- Place this tag in your head or just before your close body tag. -->
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/popper/popper.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/bootstrap.js"></script>
+    <script
+        src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js">
+    </script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/vendor/js/menu.js"></script>
+    <script src="https://presensi.pipapip.web.id/assets/vendor/sneat/assets/js/main.js"></script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <script>
       $('#myalert').delay('slow').slideDown('slow').delay(3000).slideUp(600);
